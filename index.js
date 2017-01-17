592bbe1e25e975402526753603897b4933705ac5
Caroline Wynspeare <carolinewynspeare@gmail.com>
2017-01-17 05:21:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var evens = [0, 2, 4, 6, 8, 10]
+
+evens.forEach(even => {
+  console.log(`${even} is not odd!`)
+})
+evens.forEach((even, index, array) => {
+  debugger
+  console.log(`${even} is not odd!`)
+})
+
+function square(n) {
+  console.log(n * n)
+}
+
+var evens = [0, 2, 4, 6, 8, 10]
+
+function doToEvens(callback) {
+  evens.forEach(callback)
+}
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+var animals = ["dog", "fish", "cat"]
+
+doToElementsInArray(animals, changeCompletely)
